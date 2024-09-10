177e0457ad2958e39f8dfdd4e2b8fe5f14ac8570
joe <13431761+joseph-tohdjojo@users.noreply.github.com>
2024-09-09 22:06:00 -0400

    Two Sum solution

---
+++ Lessons/Getting Started/Basic Data Structures/Basic Data Structures and Algorithms/Two Sum/README.md
@@ -0,0 +1,5 @@
+# Two Sum
+
+AlgoMonster Lesson: https://algo.monster/liteproblems/1
+
+Leet code link: https://leetcode.com/problems/two-sum/description/
---
+++ Lessons/Getting Started/Basic Data Structures/Basic Data Structures and Algorithms/Two Sum/index.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  const hashMap = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    let num = nums[i];
+    let complement = target - num;
+    let complementIndex = hashMap[complement];
+    if (complementIndex !== undefined) {
+      return [complementIndex, i];
+    }
+    hashMap[num] = i;
+  }
+};
