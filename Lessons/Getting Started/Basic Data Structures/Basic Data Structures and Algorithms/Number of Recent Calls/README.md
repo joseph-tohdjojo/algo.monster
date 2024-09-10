3c21c3bc4b0952d079f9fa7b41d5427baabba3fb
joe <13431761+joseph-tohdjojo@users.noreply.github.com>
2024-09-10 01:00:54 -0400

    Add Number of Recent Calls problem and solutions

---
+++ Lessons/Getting Started/Basic Data Structures/Basic Data Structures and Algorithms/Number of Recent Calls/README.md
@@ -0,0 +1,9 @@
+# Number of Recent Calls
+
+### AlgoMonster Lesson
+
+https://algo.monster/liteproblems/933
+
+### Leet code link
+
+https://leetcode.com/problems/number-of-recent-calls/description/
---
+++ Lessons/Getting Started/Basic Data Structures/Basic Data Structures and Algorithms/Number of Recent Calls/solution-002.js
@@ -0,0 +1,59 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  add(value) {
+    const node = new Node(value);
+    if (!this.head) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
+    this.length += 1;
+  }
+
+  removeLeft() {
+    if (this.head) {
+      this.head = this.head.next;
+      this.length -= 1;
+    }
+  }
+}
+
+class RecentCounter {
+  constructor() {
+    this.queue = new LinkedList();
+  }
+
+  /**
+   * @param {number} t
+   * @return {number}
+   */
+  ping(t) {
+    this.queue.add(t);
+
+    while (this.queue.head.value < t - 3000) {
+      this.queue.removeLeft();
+    }
+
+    return this.queue.length;
+  }
+}
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * var obj = new RecentCounter()
+ * var param_1 = obj.ping(t)
+ */
