cb3bbadb39145941dbcfbd1c9401b3cecd3b8bf5
joe <13431761+joseph-tohdjojo@users.noreply.github.com>
2024-09-11 19:20:49 -0400

    attempt without creating new arrays

--- Lessons/Getting Started/Basic Algorithms/02 - Advanced Sorting/Merge Sort/solution-001.js
+++ Lessons/Getting Started/Basic Algorithms/02 - Advanced Sorting/Merge Sort/solution-001.js
@@ -1,29 +1,38 @@
-function mergeSort(list) {
-  if (list.length === 1) return list;
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+function sortList(list) {
+  function sort(start, end) {
+    if (start === end) return [list[end]];
 
-  let mid = Math.floor(list.length / 2);
-  let leftList = sortList(list.slice(0, mid));
-  let rightList = sortList(list.slice(mid));
+    const mid = Math.floor((end - start) / 2) + start;
+    const leftList = sort(start, mid);
+    const rightList = sort(mid + 1, end);
 
-  let newList = [];
-  let l = 0;
-  let r = 0;
+    let newList = [];
+    let l = 0;
+    let r = 0;
 
-  while (newList.length < leftList.length + rightList.length) {
-    const left = leftList[l] || Infinity;
-    const right = rightList[r] || Infinity;
-    if (left < right) {
-      newList.push(left);
-      l++;
-    } else if (left > right) {
-      newList.push(right);
-      r++;
-    } else {
-      newList.push(left, right);
-      l++;
-      r++;
+    while (newList.length < leftList.length + rightList.length) {
+      const left = leftList[l] || leftList[l] === 0 ? leftList[l] : Infinity;
+      const right =
+        rightList[r] || rightList[r] === 0 ? rightList[r] : Infinity;
+      if (left < right) {
+        newList.push(left);
+        l++;
+      } else if (left > right) {
+        newList.push(right);
+        r++;
+      } else {
+        newList.push(left, right);
+        l++;
+        r++;
+      }
     }
+
+    return newList;
   }
 
-  return newList;
+  return sort(0, list.length - 1);
 }
